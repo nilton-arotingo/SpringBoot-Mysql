f566b2f6e000619324ae8ddb5f31b4226621b0aa
Nilton_AM <nilton.arotingo@gmail.com>
2018-08-11 14:21:34 -0500

    Docker-mysql - v1: simple sample like helloWorld

---
+++ docker_mysql/src/main/java/com/nto/docker_mysql/DockerMysqlApplication.java
@@ -0,0 +1,12 @@
+package com.nto.docker_mysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerMysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerMysqlApplication.class, args);
+	}
+}
---
+++ docker_mysql/src/main/java/com/nto/docker_mysql/api/HomController.java
@@ -0,0 +1,32 @@
+package com.nto.docker_mysql.api;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomController {
+	
+	@Autowired
+	Environment environment;
+
+	@RequestMapping("/")
+	public String hello() throws UnknownHostException {
+		String datePattern = "dd/MM/yyyy HH:mm:ss";
+		DateTimeFormatter df = DateTimeFormatter.ofPattern(datePattern);
+		LocalDateTime now = LocalDateTime.now();
+		
+		String hostName = InetAddress.getLocalHost().getHostName();
+		String port = environment.getProperty("local.server.port");
+			
+		return "Do you speak whale? - web server @ " + df.format(now) + ".<br><br><b>Host:</b> " 
+		+ hostName + "<span style=\"color:red;\">:</span>" + port;
+	}
+	
+}
